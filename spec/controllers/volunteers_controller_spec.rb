c56d884a5fc7fde5d226ac5ec5eee24c2c8b6091
homarinn <homarinn1230@gmail.com>
2019-08-21 09:02:16 +0900

    Update volunteers_controller_spec.rb

--- spec/controllers/volunteers_controller_spec.rb
+++ spec/controllers/volunteers_controller_spec.rb
@@ -4,13 +4,11 @@ describe VolunteersController, type: :controller do
   describe 'GET #index' do
 
     it "populates an array of volunteers ordered by created_at DESC" do
-      organization_1 = create(:organization)
-      organization_2 = create(:organization)
-      volunteer_1 = create(:volunteer)
-      volunteer_2 = create(:volunteer)
-      volunteer_3 = create(:volunteer, organization_id: organization_1.id)
-      volunteer_4 = create(:volunteer, organization_id: organization_1.id)
-      volunteer_5 = create(:volunteer, organization_id: organization_2.id)
+      organizations = create_list(:organization, 2)
+      volunteers_example = create_list(:volunteer, 2)
+      volunteer_3 = create(:volunteer, organization_id: organizations[0].id)
+      volunteer_4 = create(:volunteer, organization_id: organizations[0].id)
+      volunteer_5 = create(:volunteer, organization_id: organizations[1].id)
       volunteers = [volunteer_3, volunteer_4, volunteer_5]
       get :index
       expect(assigns(:volunteers)).to match(volunteers.sort{ |a, b| b.created_at <=> a.created_at })
